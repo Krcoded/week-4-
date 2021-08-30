303799fa08cdccce7f436675b0e788bf146ad31b
Kevin Rodriguez <87106659+Krcoded@users.noreply.github.com>
2021-08-30 12:33:12 -0400

    add

---
+++ destructuring.js
@@ -0,0 +1,31 @@
+
+const movies = {
+    title: 'Shrek 2',
+    studio: 'Dreamworks',
+    characters: [
+        {name:'Burro'},
+        {name: 'Shrek'}, 
+        {name: 'Fiona'},
+        {name:'Muneco de Gengibre'}, 
+        {name:'Dragon'},
+    ],
+    context: "Shrek' Swap",
+    durationHours: 1.3,
+    rating: "10/10",
+    rated: 'PG13ish', 
+    song: 'I need a Hero'
+
+}
+
+
+const { 
+    title, 
+    studio,
+    characters: characters, 
+    context: place, 
+    durationHours, 
+}
+
+
+const [first, section, third, fourth, fifth] = characters
+
